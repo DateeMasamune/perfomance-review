a69af4ade7dcecf6bbd22a01b7e618bd12ed7a10
Ovsienko Dmitry <ovsienkodima@mail.ru>
2024-07-31 19:34:28 +0300

    init mk on lib

---
+++ my-lib/consumer-1/src/App.tsx
@@ -0,0 +1,16 @@
+import { Footer, Header } from 'lib-origin'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className='content'>
+        Content
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ my-lib/lib-origin/src/App.tsx
@@ -0,0 +1,10 @@
+import { Footer, Header } from "./components"
+
+export const App = () => {
+    return (
+        <div>
+            <Header />
+            <Footer />
+        </div>
+    )
+}
